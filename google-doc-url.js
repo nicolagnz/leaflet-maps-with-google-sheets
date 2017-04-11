775c1cf3387e9a889968129a1906399526784ab8
nicolagnz <NICOLAOUTTEN@YAHOO.CO.UK>
2017-04-11 21:39:15 +1200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1xSZDQ91o8BJuDxvgu8fEkyrtadKLq2PSf2RgNoXg8gw/edit#gid=164271551';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1xSZDQ91o8BJuDxvgu8fEkyrtadKLq2PSf2RgNoXg8gw/edit#gid=0';
